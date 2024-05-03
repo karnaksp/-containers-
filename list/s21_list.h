0f0ea653d8e34cb58e973f87b1543731999220e0
Fyodor Bashlakov <feiaoduoit@gmail.com>
2024-05-03 17:23:42 +0700

    add comments to the list header

--- list/s21_list.h
+++ list/s21_list.h
@@ -40,52 +40,135 @@ class ListConstIterator {
 
 template<class T>
 class list {
-private:
-    Node<T>* head;
-    Node<T>* tail;
 public:
     using value_type = T;
     using reference = T&;
     using const_reference = const T&;
     using iterator = ListIterator<T>;
     using const_iterator = ListConstIterator<T>;
-    using size_type = size_t;
+    using size_type = std::size_t;
+private:
+    Node<T>* head_;
+    Node<T>* tail_;
+    size_type size_;
 
-public:
-    // List Functions
+public:    
+            /**********************************
+            *
+            *   List Functions
+            * 
+            ***********************************/ 
+
+    // default constructor, creates an empty list
     list();
+
+    // parameterized constructor, creates the list of size n
     list(size_type n);
+
+    /*
+    initializer list constructor, 
+    creates a list initizialized using std::initializer_list
+    */
     list(std::initializer_list<T> const &items);
+
+    // copy constructor
     list(const list &l);
+
+    // move constructor
     list(list &&l);
+
+    // destructor
     ~list();
+
+    // assignment operator overload for moving an object
     list operator=(list &&l);
 
-    // List Element access
+            /**********************************
+            *
+            *   List Element access
+            * 
+            ***********************************/ 
+
+    // access the first element
     const_reference front();
+
+    // access the last element
     const_reference back();
+ 
+            /**********************************
+            *
+            *   List Iterators
+            * 
+            ***********************************/ 
 
-    // List Capacity
+    // returns an iterator to the beginning
+    iterator begin();
+
+    // returns an iterator to the end
+    iterator end(); 
+ 
+            /**********************************
+            *
+            *   List Capacity
+            * 
+            ***********************************/ 
+
+    // checks whether the container is empty
     bool empty();
+
+    // returns the number of elements
     size_type size();
+
+    // returns the maximum possible number of elements
     size_type max_size();
-
-    // List Modifiers
-    void clear();
-    iterator insert(iterator pos, const_reference value);
-    void erase(iterator pos);
-    void push_back(const_reference value);
-    void pop_back();
-    void push_front(const_reference value);
-    void pop_front();
-    void swap(list& other);
-    void merge(list& other);
-    void splice(const_iterator pos, list& other);
-    void reverse();
-    void unique();
-    void sort();
-
+          
+            /**********************************
+            *
+            *   List Modifiers
+            * 
+            ***********************************/ 
     
+    // clears the contents
+    void clear();
+    
+    /*
+    inserts element into concrete pos and 
+    returns the iterator that points to the new element
+    */
+    iterator insert(iterator pos, const_reference value);
+    
+    // erases an element at pos
+    void erase(iterator pos);
+    
+    // adds an element to the end
+    void push_back(const_reference value);
+    
+    // removes the last element
+    void pop_back();
+    
+    // adds an element to the head
+    void push_front(const_reference value);
+    
+    // removes the first element
+    void pop_front();
+    
+    // swaps the contents
+    void swap(list& other);
+    
+    // merges two sorted lists
+    void merge(list& other);
+    
+    // transfers elements from list other starting from pos
+    void splice(const_iterator pos, list& other);
+    
+    // reverses the order of the elements
+    void reverse();
+    
+    // removes consecutive duplicate elements
+    void unique();
+    
+    // sorts the elements
+    void sort();
 };
 
 
